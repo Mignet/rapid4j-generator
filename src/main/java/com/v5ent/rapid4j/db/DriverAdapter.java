b0db0efc8c0aa3d59b96e5a4bc98b35cb8e70a2f
Mignet <mignetwee@gmail.com>
2015-11-14 16:55:03 +0800

    minor change

--- src/main/java/com/v5ent/rapid4j/db/DriverAdapter.java
+++ src/main/java/com/v5ent/rapid4j/db/DriverAdapter.java
@@ -12,20 +12,20 @@ public class DriverAdapter {
 		Connection conn = null;
 		try {
 			if ("MySQL".equals(db.getDbtype())) {
-				Class.forName("com.mysql.jdbc.Driver", true, Thread.currentThread().getContextClassLoader());
+				Class.forName("com.mysql.jdbc.Driver").newInstance();
 				conn = DriverManager.getConnection(
 						"jdbc:mysql://" + db.getDbhost() + ":" + db.getDbport() + "/" + db.getDbname()
 								+ "?useUnicode=true&characterEncoding=UTF-8&connectTimeout=10000&socketTimeout=180000",
 						db.getUsername(), db.getPassword());
 			} else if ("PostgreSQL".equals(db.getDbtype())) {
-				Class.forName("org.postgresql.Driver", true, Thread.currentThread().getContextClassLoader());
+				Class.forName("org.postgresql.Driver").newInstance();
 				conn = DriverManager.getConnection(
 						"jdbc:postgresql://" + db.getDbhost() + ":" + db.getDbport() + "/" + db.getDbname()
 								+ "?useUnicode=true&characterEncoding=UTF-8&loginTimeout=10&socketTimeout=180",
 						db.getUsername(), db.getPassword());
 			} else if ("SQLite".equals(db.getDbtype())) {
 				// jdbc:sqlite:dbPath/dbname.db
-				Class.forName("org.sqlite.JDBC", true, Thread.currentThread().getContextClassLoader());
+				Class.forName("org.sqlite.JDBC").newInstance();
 				conn = DriverManager.getConnection("jdbc:sqlite:db/" + db.getDbname() + ".db", db.getUsername(),
 						db.getPassword());
 			}
